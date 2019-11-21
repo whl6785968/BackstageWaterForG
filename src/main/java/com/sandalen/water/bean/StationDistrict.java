4730d430c5411a36c163f8e1995e87c69d4fc2d8
sandalen <806403789@qq.com>
2019-11-21 08:49:53 +0800

    THIRD

---
+++ src/main/java/com/sandalen/water/bean/StationDistrict.java
@@ -0,0 +1,33 @@
+package com.sandalen.water.bean;
+
+public class StationDistrict {
+    private Integer id;
+
+    private String sid;
+
+    private String did;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid == null ? null : sid.trim();
+    }
+
+    public String getDid() {
+        return did;
+    }
+
+    public void setDid(String did) {
+        this.did = did == null ? null : did.trim();
+    }
+}
